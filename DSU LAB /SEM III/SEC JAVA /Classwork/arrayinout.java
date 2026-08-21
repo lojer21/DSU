17cba09285ab69092515818f019580d1e0673bd2
LOJER PEDERAR <191953157+lojer21@users.noreply.github.com>
2026-08-22 00:05:15 +0530

    Refactor main method variable names in arrayinout
    Updated variable names for consistency and clarity.

--- DSU LAB /SEM III/SEC JAVA /Classwork/arrayinout.java
+++ DSU LAB /SEM III/SEC JAVA /Classwork/arrayinout.java
@@ -1,20 +1,17 @@
+// 25/08/2025
+// Find the sum and average of the array elemnts.
 import java.util.*;
-
 public class arrayinout
 {
-    public static void main(String[] Args)
+    public static void main(String[] aya)
     {
-        Scanner input = new Scanner(System.in);
-
+        Scanner lo= new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
-        int n = input.nextInt();
+        int n =lo.nextInt();
         int[] arr = new int[n];
-        
         System.out.printf("Enter %d of the array: ");
         for (int i = 0; i< n; i++)
+          {
             arr[i] = input.nextInt();
-
-
-    
     }
 }
