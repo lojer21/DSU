e975671ac266dd4170ee84fd81ea1eec77ec5e6a
LOJER PEDERAR <191953157+lojer21@users.noreply.github.com>
2026-08-22 00:16:03 +0530

    Create logicalnot.java

---
+++ DSU LAB /SEM III/SEC JAVA /MODULE 4/logicalnot.java
@@ -0,0 +1,11 @@
+public class logicalnot {
+    public static void main(String[]args){
+        boolean israining=false;
+          if(!israining){
+            System.out.println("you shoud take the umbrella.");
+        }else{
+            System.out.println("umbrella not need.");
+        }
+    }
+    
+}
